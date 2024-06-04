4d335a3ca61cdd4cd818b83744c41792f74bfbd3
shoebilyas123 <shoebilyas123@gmail.com>
2024-06-04 13:20:52 +0530

    auth svc connecToDB

---
+++ auth-svc/cmd/api/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"auth-svc/cmd/api/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPORT = "8080";
+
+const MaxDBConnTries int = 15;
+const RetryDBConnIn time.Duration = 5;
+
+type Config struct {
+	DB *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Booting up authentication server...");	
+	
+	conn := connectToDB();
+
+	if conn == nil {
+		log.Panic("Database connection failed.");
+	}
+
+	app := Config{
+		DB: conn,
+		Models: data.New(conn),
+	};
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s",webPORT),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe();
+
+	if err != nil {
+		log.Panic(err);
+	}
+	
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn);
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping();
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN");
+	var count int = 0;
+
+	for {
+		conn, err := openDB(dsn);
+
+		if err != nil {
+			log.Println("Postgres not ready yet");
+			count++;
+		}	else {
+			log.Println("Successfully connected to Postgres");
+			return conn;
+		}
+
+		if count > MaxDBConnTries {
+			log.Println("Couldn't connec to DB")
+			return nil;
+		}
+
+		log.Println("Retrying in 5 seconds");
+		time.Sleep(RetryDBConnIn*time.Second);
+		continue;
+	}
+}
---
+++ auth-svc/cmd/api/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://*","https://*"},
+		AllowedMethods: []string{"POST","GET","PUT","PATCH","DELETE"},
+		AllowedHeaders: []string{"Authentication","Content-Type","Accept","X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge: 300,
+	}))
+
+
+	return mux;
+}
